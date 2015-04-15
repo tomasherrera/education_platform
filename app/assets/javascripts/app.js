0bdb0cb1315cbf1e3f8173f8a148ac00d0655c36
Tomas Herrera <tomasherrera91@hotmail.com>
2015-04-15 04:32:10 -0500

    Fixed update form on index view

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,35 +6,24 @@
     $scope.courses = [];
     $scope.order = "title";
     IndexCourses = $resource("/index.json", {}, {});
-    $scope.index_courses = IndexCourses.get().$promise.then(
+
+    $scope.getCourses = function(){
+      IndexCourses.get().$promise.then(
       //success
       function( value ){
         $scope.courses = value.courses;
         $scope.assisted_courses = value.assisted_courses;
       },
       function( error ){/*Do something with error*/}
-    );
+    )};
+    $scope.getCourses();
 
-    $scope.refresh = function(){
-      $scope.index_courses = IndexCourses.get().$promise.then(
-        //success
-        function( value ){
-          $scope.courses = value.courses;
-          $scope.assisted_courses = value.assisted_courses;
-        },
-        //error
-        function( error ){/*Do something with error*/}
-      );
-    };
     var Course = $resource('/courses/:id', {id: "@id"} , {"update": {method: "PUT"}});
     $scope.update = function(course){
-      var course1 = Course.get({id: course.id});
-      var title = prompt("Enter new Course title:", course.title);
-      course1.title = title;
-      Course.update({ id:course.id }, course1).$promise.then(
+      Course.update({ id:course.id }, course).$promise.then(
         //success
         function( value ){
-          $scope.refresh();
+          $scope.getCourses();
         },
         //error
         function( error ){/*Do something with error*/}
